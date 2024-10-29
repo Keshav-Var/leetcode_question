a0abb45d82dda3660fc5387ff263726c68a49279
Keshav Varshney <147650289+Keshav-Var@users.noreply.github.com>
2024-10-29 22:38:58 +0530

    Time: 16 ms (54.35%), Space: 74.5 MB (43.37%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
+    static int countMoves(vector<vector<int>>& grid, int prevData, int row, int col, vector<vector<int>>& dp){
+        int m = grid.size();
+        int n = grid[0].size();
+        if(row < 0 || col < 0 || row >= m || col >= n)
+        return 0;
+        if(grid[row][col] <= prevData)
+        return 0;
+        if(dp[row][col] != -1)
+        return dp[row][col];
+        dp[row][col] = 1 + max({countMoves(grid, grid[row][col], row-1, col+1, dp), countMoves(grid, grid[row][col], row, col+1, dp), countMoves(grid, grid[row][col], row+1, col+1, dp)});
+        return dp[row][col];
+    }
+
     int maxMoves(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        unordered_set<int> q, t;
-        for (int i = 0; i < m; ++i) {
-            q.insert(i);
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        cout<<m<<" "<<n;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        for(int i = 0; i < m; i++){
+            // dp = std::vector<std::vector<int>>(m, std::vector<int>(n, -1));
+            ans = max(ans, countMoves(grid, -1, i, 0, dp));
         }
-        for (int j = 0; j < n - 1; ++j) {
-            t.clear();
-            for (int i : q) {
-                for (int k = i - 1; k <= i + 1; ++k) {
-                    if (k >= 0 && k < m && grid[i][j] < grid[k][j + 1]) {
-                        t.insert(k);
-                    }
-                }
-            }
-            if (t.empty()) {
-                return j;
-            }
-            q.swap(t);
-        }
-        return n - 1;
+        return ans-1;
     }
 };
