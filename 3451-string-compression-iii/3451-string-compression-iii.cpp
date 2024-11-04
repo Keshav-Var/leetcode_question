9ecd596b853594dc4373b128d48887f4099c79d5
Keshav Varshney <147650289+Keshav-Var@users.noreply.github.com>
2024-11-04 09:25:45 +0530

    Time: 70 ms (15.38%), Space: 30 MB (24.06%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     string compressedString(string word) {
-        for(int i = word.length()-1; i >= 0;){
+        string comp = "";
+        for(int i = 0; i < word.length();){
             char ch = word[i];
             int freq = 0;
-            int si = i;
-            while(i >= 0 && word[i] == ch){
-                i--;
+            while(i < word.length() && word[i] == ch && freq < 9){
+                i++;
                 freq++;
             }
-            string temp = "";
-            while(freq > 0){
-                if(freq > 9){
-                    temp = temp + "9" + ch;
-                    freq -= 9;
-                }else{
-                    temp = temp + to_string(freq) + ch;
-                    freq = 0;
-                }
-            }
-            word.replace(i+1, si-i, temp);
+            comp += to_string(freq) + ch;
         }
-        return word;
+        return comp;
     }
 };
