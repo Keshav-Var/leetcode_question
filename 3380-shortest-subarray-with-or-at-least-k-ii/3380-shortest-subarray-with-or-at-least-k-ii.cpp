f129d5ade64a15838226f46ffc8093a676c32899
Keshav Varshney <147650289+Keshav-Var@users.noreply.github.com>
2024-11-10 22:46:14 +0530

    Time: 67 ms (53.85%), Space: 88 MB (97.77%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    void updateBits(vector<int>& bits, int num, int diff){
+    int updateBits(vector<int>& bits, int num, int diff){
+        int ans = 0;
         for(int i = 0; i < 32; i++){
             if(num & (1<<i))
             bits[i] += diff;
-        }
-        cout<<endl;
-    }
-
-    int findOr(vector<int>& bits){
-        int ans = 0;
-        for(int i = 0; i < 32; i++){
             if(bits[i])
             ans = ans | (1<<i);
         }
         return ans;
     }
+
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int i = 0, ans = INT_MAX;
+        int orVal;
         vector<int> bits(32, 0);
         for(int j = 0; j < nums.size(); j++){
-            updateBits(bits, nums[j], 1);
-            while(i <= j && findOr(bits) >= k){
+            orVal = updateBits(bits, nums[j], 1);
+            while(i <= j && orVal >= k){
                 ans = min(ans, j - i + 1);
-                updateBits(bits, nums[i], -1);
+                orVal = updateBits(bits, nums[i], -1);
                 i++;
             }
         }
